4f70cd5af4375b994a6fb20d9dea486d5b7cd1c0
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-10-23 16:17:43 +0530

    Knapsack Using Dynamic Programming

---
+++ knapsack_dp.py
@@ -0,0 +1,22 @@
+
+#Knapsack Problem
+
+value = [20, 5, 10, 40, 15, 25]
+weight = [1, 2, 3, 8, 7, 4]
+w = 10
+l = len(value)
+
+dp = [[0 for i in range(w+1)]for j in range(l+1)]
+
+for i in range(1,l+1):
+    for j in range(1,w+1):
+            
+        if weight[i-1] <= j:
+            dp[i][j] = max(dp[i-1][j], value[i-1] + dp[i-1][j-weight[i-1]])
+            
+        else:
+            dp[i][j] = dp[i-1][j]
+            
+print("Maximum Possible Value : ",dp[l][w])
+        
+
