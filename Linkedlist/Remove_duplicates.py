fe8d8059e18df094b3a9a1e127d9a2b78bd54df2
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-12-01 13:28:10 +0530

    Remove duplicates
    if consecutive elements are same, then point next to next.next, else just traverse through the linkedlist

---
+++ Linkedlist/Remove_duplicates.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        temp = head
+        
+        while temp and temp.next:
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            
+            else:
+                temp = temp.next
+            
+            
+        return head
