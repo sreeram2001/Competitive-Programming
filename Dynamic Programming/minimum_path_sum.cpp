2b97e54dd7bbe7a26b2245446e05a03f00054f36
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-12-16 21:17:06 +0530

    Create minimum_path_sum.cpp
    Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
    
    Note: You can only move either down or right at any point in time.
    
    Link : https://leetcode.com/problems/minimum-path-sum/

---
+++ Dynamic Programming/minimum_path_sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+        
+        int dp[m][n];
+        dp[0][0] = grid[0][0];
+ 
+        
+        if(m==1)
+        {
+            count = accumulate(grid[0].begin(), grid[0].end(), 0);
+            return count;
+        }
+              
+        
+        for(int i=1;i<n;i++)
+        {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        count = dp[m-1][n-1];
+        return count;
+        
+    }
+};
