b6315600a812df5c2b249ace16f4aaa1bd2e2e67
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-12-22 21:33:25 +0530

    Create insertion_bst.py

---
+++ Binary Search Trees/insertion_bst.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        if root == None:
+            root = TreeNode(val)
+            
+        elif val > root.val:
+            if root.right != None:
+                self.insertIntoBST(root.right,val)
+            else:
+                root.right = TreeNode(val)
+                
+        else:
+            if root.left != None:
+                self.insertIntoBST(root.left,val)
+            else:
+                root.left = TreeNode(val)
+                
+
+        return root
