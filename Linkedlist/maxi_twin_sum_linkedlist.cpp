e36d25a9bd11d3b076e73c113abf30d46f13637b
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2022-02-26 22:44:22 +0530

    Create maxi_twin_sum_linkedlist.cpp
    Given the head of a linked list with even length, return the maximum twin sum of the linked list.

---
+++ Linkedlist/maxi_twin_sum_linkedlist.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        
+        vector<int> arr;
+        int maxi = 0;
+        
+        while(head)
+        {
+            arr.push_back(head->val);
+            head = head->next;  
+        }
+        
+        for(int i=0;i<arr.size()/2;i++)
+        {
+            maxi = max(maxi,arr[i] + arr[arr.size()-i-1]);
+        }
+        
+        return maxi;
+
+    }
+};
