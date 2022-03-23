ee5f4a52b33a45628eecdc451c50c5da25f447a0
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2022-03-23 22:53:41 +0530

    Create combination_sum_4.cpp
    Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
    
    The test cases are generated so that the answer can fit in a 32-bit integer.

---
+++ Dynamic Programming/combination_sum_4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        int dp[target+1];
+        
+        for(int i=0;i<target+1;i++)
+        {
+            dp[i] = 0;
+        }
+        
+        dp[0] = 1;
+        
+        for(int i=1;i<target+1;i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[j] <= i)
+                {
+                    dp[i] = dp[i] + dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
